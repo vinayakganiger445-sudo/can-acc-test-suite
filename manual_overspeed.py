261145966f67eb7388ec4a392fdfdf2a9c70f787
Vinayak Ganiger <vinayak.ganiger0002@gmail.com>
2026-01-13 16:01:44 +0100

    Manual overspeed test - virtual CAN bus

--- manual_overspeed.py
+++ manual_overspeed.py
@@ -1,5 +1,11 @@
 import can
-bus = can.interface.Bus()  # Virtual CAN bus
-overspeed = can.Message(arbitration_id=0x101, data=[255,255,10,0,0,0,0,0])  # 120kmh
+
+bus = can.interface.Bus(interface='virtual', channel='test1')  
+
+# OVERSPEED message (ID 0x101 = Speed, 120kmh)
+overspeed = can.Message(arbitration_id=0x101, data=[255,255,10,0,0,0,0,0])
 bus.send(overspeed)
-print("✅ Manual OVERSPEED injected - test would PASS now")
+print("✅ Manual OVERSPEED injected - 120kmh on virtual CAN bus")
+print("This would make test_overspeed() PASS")
+
+bus.shutdown()  
